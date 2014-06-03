ffbaedf79d1471fc899d67ef4108e3037f5bd21a
Patrick Conley <patrick.bj.conley@gmail.com>
2014-06-03 16:15:39 -0700

    fish: don't draw the prompt on batch sessions

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,14 @@
-touch /home/pconley/temp/fish-reload
+touch /tmp/fish-reload
 
-# Prompts call __prompt_set_cwd only when dir changes: must be set explicitly
-# Draw the prompt first to set global variables
-fish_prompt > /dev/null
-set -g prompt_cwd (__prompt_set_cwd)
+if status --is-interactive
+   # Prompts call __prompt_set_cwd only when dir changes: must be set explicitly
+   # Draw the prompt first to set global variables
+   fish_prompt > /dev/null
+   set -g prompt_cwd (__prompt_set_cwd)
+end
 
 if status --is-login
-   set PATH $HOME/bin/scripts $PATH
+   set PATH $HOME/bin $HOME/bin/scripts $PATH
 end
 
 function perlmod
